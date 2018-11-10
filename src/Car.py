490264bf03ae7b7e286e9d999e0099eb54513377
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:36:22 +0900

    Create Car class

---
+++ src/Car.py
@@ -0,0 +1,16 @@
+class Car:
+    '''
+    car_num : str
+    location : str
+    status : int
+    in_time : str
+    out_time : int
+
+    ----------------------
+    get+위 모든 data fields
+    set+위 모든 data fields
+    in()
+    out()
+    park()
+    move()
+    '''

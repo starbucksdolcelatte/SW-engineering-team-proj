146b5539f10f12389e79d852057bfb013473f98d
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:37:20 +0900

    Create Camera class

---
+++ src/Camera.py
@@ -0,0 +1,12 @@
+class Camera:
+'''
+cam_num : str
+
+
+proc_vrn(image): string
+proc_zone_now(image): string list
+proc_zone_status(DB): string list
+
+get+위 모든 data fields
+set+위 모든 data fields
+'''

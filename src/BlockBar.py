a3ff2e064df1e98fbd5075019fa8e7a8d99b200f
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:40:20 +0900

    Create BlockBar class

---
+++ src/BlockBar.py
@@ -0,0 +1,12 @@
+class BlockBar:
+    '''
+    open: bool
+
+    ---------------------------------
+    open(is_rgstrd : bool):None
+    is_registered(car_num:str) : bool
+    close(is_pssd:bool):None
+    get+위 모든 data fields
+    set+위 모든 data fields
+
+    '''

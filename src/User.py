9cf70b87bb4c99b36e028ffe133ad9548947ddc5
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:30:37 +0900

    Create User clasS

---
+++ src/User.py
@@ -0,0 +1,22 @@
+ class User:
+      def __init__(self, param):
+          '''
+          car_num : str
+        card_info : str list
+        name : str
+        phone : str
+        mall_cost : int
+        mall_pay_time : str
+        park_cost : int
+        is_paid_park : bool
+        park_pay_time : str
+        free_park : int
+
+
+        pay(park_cost: int) : is_paid_park : bool
+        get+위 모든 data fields
+        set+위 모든 data fields
+
+          '''
+          # something
+          return None

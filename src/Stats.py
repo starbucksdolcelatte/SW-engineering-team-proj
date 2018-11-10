d79d60d33c698cc5083649a55d0b6c0782213df5
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:43:31 +0900

    Create Stats class

---
+++ src/Stats.py
@@ -0,0 +1,21 @@
+class Stats:
+    
+'''
+id,pw는 웹에서 구현될거라 뺐음.
+이 클래스 다이어그램은 파이썬 모듈만 포함이 됨.
+sdate:str
+edate:str
+criteria: int
+
+get_stats(criteria):int list
+
+
+mon_stats(sdate, edate):int list
+
+
+day_stats(sdate, edate):int list
+hour_stats(sdate, edate):int list
+get+위 모든 data fields
+set+위 모든 data fields
+
+'''

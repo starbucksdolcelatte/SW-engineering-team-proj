ec0261ab362586649dc79526270384f9cc39dfe1
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:42:26 +0900

    Create Kiosk class

---
+++ src/Kiosk.py
@@ -0,0 +1,14 @@
+class Kiosk:
+'''
+staff_tel:str
+car_num:str
+
+
+
+get_location(car_num:str):str
+get_car_num():car_num:str
+get_staff_tel():staff_tel:str
+set_car_num(str):none
+set_staff_tel(str):none
+
+'''

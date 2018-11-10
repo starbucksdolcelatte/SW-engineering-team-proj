dd6dfc8ff980c9e45a09f57a828e36f1e08894a5
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-11 02:41:29 +0900

    Create WeightSensor class

---
+++ src/WeightSensor.py
@@ -0,0 +1,14 @@
+class WeightSensor:
+    '''
+weight: int
+is_car: bool
+is_passed: bool
+
+
+get_weight(무게data):int
+judge_car():bool
+pass():bool
+get+위 모든 data fields
+set+위 모든 data fields
+
+    '''

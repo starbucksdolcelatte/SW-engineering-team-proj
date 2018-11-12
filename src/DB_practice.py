8caf16f06b7508dc02b07e64be86da07a53d8072
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-12 09:39:38 +0900

    Create DB Practice File :-)

---
+++ src/DB_practice.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# SQLite DB 연결
+conn = sqlite3.connect("test.db")
+
+# Connection 으로부터 Cursor 생성
+cur = conn.cursor()
+# Create
+cur.execute("CREATE TABLE kakao(Date text, Open int, High int, Low int, Closing int, Volumn int)")
+# Insert
+cur.execute("INSERT INTO kakao VALUES('16.06.03', 97000, 98600, 96900, 98000, 321405)")
+# SQL 쿼리 실행
+cur.execute("select * from kakao")
+
+# 데이타 Fetch
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+# Connection 닫기
+conn.close()

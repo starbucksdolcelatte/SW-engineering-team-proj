2a54cfcd713207b364ad04d88acca3290517cc0e
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-22 11:18:25 +0900

    Initialize CUSTOMER_LIST

--- src/PMS_db_init.py
+++ src/PMS_db_init.py
@@ -1,6 +1,9 @@
 import sqlite3
 from datetime import datetime, date
 import string
+import random
+
+random.seed(0)
 
 # SQLite DB 연결
 # test.db가 있으면 연결, 없으면 새로 생성
@@ -15,10 +18,49 @@ cur = conn.cursor()
 ## Parking_status : 0 == vacant ; 1 == occupied ; 2 == moving_out
 ## Total 3 levels, A-J zones, 1-10 spots
 ## ['B3-J10', 0, NULL]
-for floor in range (1,4):
-    for i in range (0,10):
-        for j in range (1,11):
-            cur.execute("INSERT INTO PARKINGLOT_LIST VALUES('B"+str(floor)+"-"+string.ascii_uppercase[i]+str(j)+"', 0, NULL)")
+def init_parkinglot_list():
+    for floor in range (1,4):
+        for i in range (0,10):
+            for j in range (1,11):
+                cur.execute("INSERT INTO PARKINGLOT_LIST VALUES('B"+str(floor)+"-"+string.ascii_uppercase[i]+str(j)+"', 0, NULL)")
+    return 0
+
+
+## CUSTOMER_LIST = {Customer_car_num, Customer_card_info, Customer_name}
+h_plate = ['가','나','다','라','마',
+                 '거','너','더','러','머','버','서','어','저',
+                 '고','노','도','로','모','보','소','오','조',
+                 '구','누','두','루','무','부','수','우','주',
+                 '아','바','사','자',
+                 '배','하','허','호']
+
+n_first = ['KIM','LEE', 'PARK', 'KONG', 'YOON', 'HYUN', 'KANG', 'JEONG', 'MOON', 'CHA', 'HAN', 'HWANG', 'HA']
+n_middle = ['SEO', 'MOON', 'YOON', 'KYUNG', 'HYUN', 'MIN', 'SOO', 'EUN', 'SEONG', 'JI']
+n_last = ['JOON', 'MI', 'AH', 'JOO', 'YEON', 'HYUK', 'SEOK','WOON']
+
+card = ['VISA', 'MasterCard', 'Amex', 'BC', 'UnionPay','JCB']
+
+def init_customer_list():
+    for i in range (0,5):
+        for j in range (0,9):
+            cur.execute("INSERT INTO CUSTOMER_LIST VALUES('"
+            + str(i) + str(j) + h_plate[random.randrange(len(h_plate))] + str(random.randrange(1000,9999)) +
+            "','" + card[random.randrange(len(card))] + " " + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) +
+            "','" + n_first[random.randrange(len(n_first))] + " " + n_middle[random.randrange(len(n_middle))] + " " + n_last[random.randrange(len(n_last))] +
+            "')")
+    return 0
+
+
+## SHOPPING_PAY = {Spay_id, Customer_car_num, Shopping_pay_amount, Shopping_pay_time}
+def init_shopping_pay():
+    for i in range (0,50):
+        for j in range (0,9):
+            cur.execute("INSERT INTO SHOPPING_PAY VALUES('"
+            + str(i) + str(j) + h_plate[random.randrange(len(h_plate))] + str(random.randrange(1000,9999)) +
+            "','" + card[random.randrange(len(card))] + " " + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) + "-" + str(random.randrange(1000,9999)) +
+            "','" + n_first[random.randrange(len(n_first))] + " " + n_middle[random.randrange(len(n_middle))] + " " + n_last[random.randrange(len(n_last))] +
+            "')")
+    return 0
 
 
 # SQL 쿼리 실행

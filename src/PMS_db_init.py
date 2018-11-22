bab8cca604a7049fe3c02b7b7cbf319c5f6e2207
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-22 10:09:55 +0900

    Initialize PARKINGLOT_LIST

--- src/PMS_db_init.py
+++ src/PMS_db_init.py
@@ -1,5 +1,6 @@
 import sqlite3
 from datetime import datetime, date
+import string
 
 # SQLite DB 연결
 # test.db가 있으면 연결, 없으면 새로 생성
@@ -9,8 +10,15 @@ conn = sqlite3.connect("PMS_db.sqlite",detect_types=sqlite3.PARSE_DECLTYPES)
 cur = conn.cursor()
 
 # Insert
-for i in range (1,9):
-    cur.execute("INSERT INTO PARKINGLOT_LIST VALUES('B1-A"+str(i)+"', 0, NULL)")
+
+## PARKINGLOT_LIST = {Parking_spot, Parking_status, Customer_car_num}
+## Total 3 levels, A-J zones, 1-10 spots
+for floor in range (1,4):
+    for i in range (0,10):
+        for j in range (1,11):
+            cur.execute("INSERT INTO PARKINGLOT_LIST VALUES('B"+str(floor)+"-"+string.ascii_uppercase[i]+str(j)+"', 0, NULL)")
+
+
 # SQL 쿼리 실행
 cur.execute("select * from PARKINGLOT_LIST")
 

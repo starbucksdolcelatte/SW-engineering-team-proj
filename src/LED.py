3a24130b31ce344738affb20c263dc44d5e24167
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-22 10:18:14 +0900

    Create LED class

---
+++ src/LED.py
@@ -0,0 +1,13 @@
+class LED:
+    '''
+    led_color : str
+    ----------------------
+    get_led_color
+    set_led_color:
+        if (Parking_status from PARKINGLOT_LIST) == 0 :
+            led_color = GREEN
+        elif (Parking_status from PARKINGLOT_LIST) == 1 :
+            led_color = RED
+        elif (Parking_status from PARKINGLOT_LIST) == 2 :
+            led_color = YELLOW
+    '''

ebc93bc17454848df32ac815a55eff6008c2812d
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-28 22:17:59 +0900

    Update BlockBar Class by MJ

--- src/BlockBar.py
+++ src/BlockBar.py
@@ -1,4 +1,4 @@
-    '''
+'''
     open: bool
 
     ---------------------------------
@@ -8,23 +8,27 @@
     get+위 모든 data fields
     set+위 모든 data fields
 
-    '''
-# 차단바 제어 프로세스(BlockBar.py)
+'''
+# 차단바 제어 프로세스 중 입차 관련
+# 문제: 데이터필드인 car_num은 어디서 오는가...
 
-from car import Car # 파일 이름 car.py, 클래스 이름 Car
-from W_Sensor import w_sensor # 파일 이름 W_Sensor, 클래스 이름 w_sensor
+from User import User
+from WeightSensor import WeightSensor
+
+class BlockBarIn:
 
 
-class BlockBar:
+    def __init__(self, car_num):
+        self.open = False  #초기 상태 '차단바 closed'
+        self.is_rgstrd = False  #초기 상태 '회원이 아님'
+        self.car_num = car_num  #이렇게 하는건지 모르겠음
+        self.is_pssd = False  #초기 상태 '차량이 아직 안 지나감'
 
-
-    def __init__(self):
-        self.open = False  #초기 상태 closed
-
-    def is_registered(self, car_num, is_rgstrd):
-        if (self.car_num == 등록된 회원이면):
+    def is_registered(self, car_num):
+        if (self.car_num == User.car_num):
             print("등록된 회원입니다.")
             self.is_rgstrd = True
+            return self.is_rgstrd
         else:
             print("등록된 회원이 아닙니다.")
 
@@ -36,10 +40,19 @@ class BlockBar:
             print("차단바를 열 수 없습니다.")
 
     def close(self, is_pssd):
-        if (self.is_pssd == 차량이 완전히 지나갔다면):
+        if (w_sensor.is_passed = True): 센서에서 차량이 지나갔다고 판단되면
+            self.is_pssd = True
+        if (self.is_pssd == True):
             print("차단바가 닫힙니다.")
             self.open = False
+        else:
+            print("빨리 지나가세요.")
 
-    def getOpen:
+    def getOpen(self):
+        return self.open, self.is_rgstrd, self.car_num, self.is_pssd
 
-    def setOpen:
+    def setOpen(self, open):
+        self.open = open
+        self.is_rgstrd = is_rgstrd
+        self.car_num = car_num
+        self.is_pssd = is_pssd

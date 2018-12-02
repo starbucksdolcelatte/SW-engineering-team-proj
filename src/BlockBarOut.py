1c5916c18e502e5c37f2a17bb06b5738ea3385b3
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-12-03 01:37:59 +0900

    Update BlockBarOut class on Nov 29 by MJ

--- src/BlockBarOut.py
+++ src/BlockBarOut.py
@@ -1,20 +1,28 @@
 # 차단바 제어 프로세스 중 출차 관련
 
-from User import User
+import sqlite3
 from WeightSensor import WeightSensor # 파일 이름 W_Sensor, 클래스 이름 w_sensor
 
 class BlockBarOut:
 
 
+    conn = sqlite3.connect("PMS_db.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
+    cur = conn.cursor()
+
     def __init__(self, car_num):
-        self.open = False  #초기 상태 '차단바 closed'
+        self.open = False  #초기 상태 = 차단바 is closed
         self.car_num = car_num  #이렇게 하는건지 모르겠음..
-        self.is_pssd = False  #초기 상태 '차단바 opened/차량이 아직 안 지나감'
+        self.is_pssd = False  #초기 상태 = 차단바 is opened/차량이 아직 안 지나감
 
     def open(self, is_paid):
-        if (User.is_paid_park == True):
+        curs.execute("select * from park_pay where Park_is_paid = ?",
+                     (self.paid,))
+        cust_info = curs.fetchone()
+        print(cust_info)
+        if (cust_info_two is not None):
             print("차단바가 열립니다.")
             self.open = True
+            return self.open
         else:
             print("차단바를 열 수 없습니다.")
 
@@ -34,3 +42,6 @@ class BlockBarOut:
         self.open = open
         self.car_num = car_num
         self.is_pssd = is_passd
+
+
+conn.close()

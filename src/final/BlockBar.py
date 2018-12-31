473e5ecf5260d1416dec47b5d2ce9d50338360a1
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-01-01 03:04:39 +0900

    Blockbar control class

--- src/final/BlockBar.py
+++ src/final/BlockBar.py
@@ -20,14 +20,14 @@ class BlockBar:
     is_passed(self) # 초음파 센서를 이용해 차량이 지나갔는지 여부 반환
     '''
     def __init__(self, db_path):
-        print("BlockBarIn 객체가 생성되었습니다.")
+        print("BlockBar 객체가 생성되었습니다.")
 
         # SQLite DB 연결
-        self.__mypmsdb = DBinit(db_path)
+        self.mypmsdb = DBinit(db_path)
         self.__open = False  #초기 상태 = 차단바 closed
         self.__is_rgstrd = False  #초기 상태 = 회원이 아님
         self.__is_pssd = False  #초기 상태 = 차량이 아직 안 지나감
-        self.__paid = False  #초기 상태 = 두번째 방문 차량에 해당됨
+        self.__paid = False
         self.__WIDTH = 200 # 입구의 너비. 입구에 차량이 없을 때 200cm
         self.__dist = self.__WIDTH # 물체와 초음파 센서 사이의 거리
 
@@ -65,23 +65,28 @@ class BlockBar:
 
     # 미납요금 결제
     def pay(self, car_num):
+
         # 결제 모듈이 들어가야 함.
         # 결제 시스템과의 통합 필요.
         self.mypmsdb.cur.execute("""SELECT Park_is_paid, Park_pay_amount, Park_out
                                     FROM park_pay WHERE customer_car_num = ?""",
                                     (car_num,))
-        cust_info = self.mypmsdb.cur.fetchall()
+        cust = self.mypmsdb.cur.fetchall()
+        cust_info = []
+        for row in cust:
+            cust_info.append(list(row))
 
+        print(cust_info)
         # 미납요금에 대하여 결제
-        for i in range(len(cust_info)):
-            if (cust_info[i][0] == 0):
+        for row in cust_info:
+            if (row[0] == 0):
                 self.mypmsdb.cur.execute("""UPDATE park_pay
                                             SET Park_is_paid = 1
                                             WHERE Park_out = ?""",
-                                            (cust_info[i][2]))
+                                            (row[2],))
                 # commit 을 해줘야 sqlite 에 반영이 됨
                 self.mypmsdb.conn.commit()
-                print(cust_info[i][2], ' ', cust_info[i][1],
+                print(row[2], ' ', row[1],
                 ' 원에 대하여 결제가 완료되었습니다.')
                 self.paid = True
 
@@ -93,30 +98,33 @@ class BlockBar:
                 print("차단바가 열립니다.")
                 self.open = True
             else:
-                self.mypmsdb.cur.execute()
                 print("미납요금이 결제됩니다.")
+                self.pay(car_num)
+                print("차단바가 열립니다.")
+                self.open = True
         else:
             self.open = False
             print("비회원에게 차단바를 열 수 없습니다.")
 
 
     def blockbar_close(self, t_pssd):
-        if (self.is_passed(t_pssd) == True): #센서에서 차량이 지나갔다고 판단되면
+        self.is_pssd = self.is_passed(t_pssd)
+        if (self.is_pssd == True): #센서에서 차량이 지나갔다고 판단되면
             print("차단바가 닫힙니다.")
             self.open = False
-        elif (self.is_passed(t_pssd) == False): # 아직 차량이 완전히 통과 못함
+            return
+        elif (self.is_pssd == False): # 아직 차량이 완전히 통과 못함
             print("빨리 지나가세요.")
             self.open = True
+            return
 
 
     def is_passed(self, t_pssd):
         if(t_pssd == True):
             self.__dist = Usonic.get_dist(198, 200)
-            print(self.__dist)
-            self.is_pssd = True
+            print("센서와 물체와의 거리 : ", self.__dist)
             return True
-        if(t_pssd == False):
+        elif(t_pssd == False):
             self.__dist = Usonic.get_dist(10, 40)
-            print(self.__dist)
-            self.is_pssd = False
+            print("센서와 물체와의 거리 : ", self.__dist)
             return False
